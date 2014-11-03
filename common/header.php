3791bff9357acf1a9cf8819ef4202d2fe143e976
hskettler <hannahskettler@gmail.com>
2014-11-03 17:28:46 -0600

    styling and path fixes

--- common/header.php
+++ common/header.php
@@ -55,8 +55,8 @@
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
-                  <!--  <li>Data for Researchers</li>
-                    <li>Lesson-plans for Teachers</li> -->
+                    <li><a href="/data">Data & Resources</a></li>
+                   <!-- <li>Lesson-plans for Teachers</li> -->
                     <li><a href="/contact">Contact</a></li>
 					<!-- <li><a href="/map">Station Map</a></li> -->
                 </ul>
