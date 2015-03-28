c58f81cad8c24ea278032a6f7e82aaf234fd4ba5
hskettler <hannahskettler@gmail.com>
2015-03-28 14:35:22 -0500

    narrative update
    update to Abigail narrative after consultation with Don Gurnett; changed
    made to web and mobile version

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,7 @@
 		<div id="sources">
 
 				<header><h2>Sources and Credits</h2></header>
-				<p>Digitization and preservation of the Explorer 1 audio data-tapes, archival material and the construction of this website made possible by a generous grant from the Roy J. Carver Charitable Trust.</p>
+				<p>Digitization and preservation of the Explorer 1 audio data tapes, archival material and the construction of this website made possible by a generous grant from the Roy J. Carver Charitable Trust.</p>
 				<p>Website narrative by Abigail Foerstner. Content for many historic episodes draws on her biography, <cite>James Van Allen: The First Eight Billion Miles</cite> (University of Iowa Press).
 				</p>
 				<p id="bottom-anchor">All of the University of Iowa archival material presented on this site is available in the <a href="http://digital.lib.uiowa.edu/cdm/search/collection/vanallen" target="_blank">Iowa Digital Library James Van Allen collection</a></p>
